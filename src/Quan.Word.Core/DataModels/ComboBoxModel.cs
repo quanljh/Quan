24dff8ddf51b775cca49137ec1d6005a4fa060e4
quanljh <quanljh@gmail.com>
2020-08-05 16:18:25 +0800

    Add enum extension

---
+++ src/Quan.Word.Core/DataModels/ComboBoxModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Mvvm;
+
+namespace Quan.Word.Core
+{
+    public class ComboBoxModel : BindableBase
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        private int _value;
+
+        public int Value
+        {
+            get => _value;
+            set => SetProperty(ref _value, value);
+        }
+
+
+        /// <summary>
+        /// コード
+        /// </summary>}
+        private string _code;
+
+        public string Code
+        {
+            get => _code;
+            set => SetProperty(ref _code, value);
+        }
+
+
+        /// <summary>
+        /// 名前
+        /// </summary>
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+    }
+}
---
+++ src/Quan.Word.Core/Enum/EnumExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quan.Word.Core.Enum
+{
+    public static class EnumExtension
+    {
+        #region Attribute
+
+        [AttributeUsage(AttributeTargets.Field)]
+        public sealed class CodeAttribute : Attribute
+        {
+            public string Code;
+            public CodeAttribute(string code)
+            {
+                Code = code;
+            }
+        }
+
+        [AttributeUsage(AttributeTargets.Field)]
+        public sealed class NameAttribute : Attribute
+        {
+            public string Name;
+            public NameAttribute(string name)
+            {
+                Name = name;
+            }
+        }
+
+        #endregion
+
+        #region Get Accessor
+
+        public static string GetCode(this System.Enum value)
+        {
+            var typeInfo = value.GetType();
+            var attributes = typeInfo.GetCustomAttributes(typeof(CodeAttribute), false).Cast<CodeAttribute>().ToArray();
+            if ((attributes?.Count() ?? 0) <= 0)
+                return null;
+            return attributes[0].Code;
+        }
+
+        public static string GetName(this Enum value)
+        {
+            var typeInfo = value.GetType();
+            var attributes = typeInfo.GetCustomAttributes(typeof(NameAttribute), false).Cast<NameAttribute>().ToArray();
+            if ((attributes?.Count() ?? 0) <= 0)
+                return null;
+            return attributes[0].Name;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static List<T> EnumToList<T>(Type enumType) where T : ComboBoxModel, new()
+        {
+            if (enumType.BaseType != typeof(Enum))
+                return null;
+
+            var result = new List<T>();
+            foreach (var enumValue in enumType.GetEnumValues())
+            {
+                var fieldInfo = enumType.GetField(enumType.GetEnumName(enumValue));
+                if (fieldInfo == null)
+                    continue;
+                var code = (CodeAttribute)Attribute.GetCustomAttribute(fieldInfo, typeof(CodeAttribute));
+                var name = (NameAttribute)Attribute.GetCustomAttribute(fieldInfo, typeof(NameAttribute));
+                T obj = new T()
+                {
+                    Code = code?.Code,
+                    Name = name?.Name ?? enumValue.ToString(),
+                    Value = Convert.ToInt32(enumValue)
+                };
+
+                result.Add(obj);
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
